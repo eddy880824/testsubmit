d8b4006210e8f89cbeb9e2eede34524ff03c3dd1
zhangsixue <zhangsixue@xiaomi.com>
2022-02-28 11:45:48 +0800

    test hook

--- src/1.js
+++ src/1.js
@@ -1,6 +1,15 @@
-const a = "1"
-const b = '2';const c = "#";
-
+const a = "1";
+const b = "2";
+const c = "#";
 const d = a + b + c;
 
-console.log(d)
+console.log(d);
+function e(x1, x2, x3) {
+  console.log(x1, x2, x3);
+}
+
+e(
+  function () {},
+  function () {},
+  function () {}
+);
