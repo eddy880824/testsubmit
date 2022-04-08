bade435a875eab8df07389d55b07e0abf13105fe
zhangsixue <zhangsixue@xiaomi.com>
2022-04-08 10:52:23 +0800

    feat: 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-    // "env": {
-    //     "browser": true,
-    //     "es2021": true
-    // },
-    // "extends": [
-    //     "eslint:recommended",
-    //     // "plugin:react/recommended"
-    // ],
-    // "parserOptions": {
-    //     "ecmaFeatures": {
-    //         "jsx": true
-    //     },
-    //     "ecmaVersion": "latest",
-    //     "sourceType": "module"
-    // },
-    // "plugins": [
-    //     // "react"
-    // ],
-    // "rules": {
-    // }
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        // "eslint:recommended",
+        "eslint-config-airbnb"
+    ],
+    "plugins": [
+        // "react"
+    ],
+    "rules": {
+    }
 }
